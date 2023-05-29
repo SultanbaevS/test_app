2bdb6a18f46f2cc930bb84304f1b835018a4b1b4
SultanbaevS <saidtasul99@gmail.com>
2023-05-29 18:37:26 +0500

    add print triangle

--- bin/lesson_5.dart
+++ bin/lesson_5.dart
@@ -1,1 +1,7 @@
-void main(List<String> arguments) {}
+import 'dart:io';
+
+void main(List<String> arguments) {
+  for (int i = 1, j = 5; i <= 5; j >= 1, i++, j--) {
+    stdout.write('${" " * j}${" *" * i}\n');
+  }
+}
